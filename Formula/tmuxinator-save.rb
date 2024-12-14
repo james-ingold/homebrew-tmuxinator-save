1fe1f4d33a4cb83b40d3d89793e0cf7d7c01cd57
James Ingold <jamesp.ingold@gmail.com>
2024-12-13 23:22:18 -0600

    fix github repo link

--- Formula/tmuxinator-save.rb
+++ Formula/tmuxinator-save.rb
@@ -1,6 +1,6 @@
 class TmuxinatorSave < Formula
   desc "CLI tool to save tmux sessions as tmuxinator configuration files"
-  homepage "https://github.com/jamessingold/tmuxinator-save"
+  homepage "https://github.com/jamesingold/tmuxinator-save"
   url "https://registry.npmjs.org/tmuxinator-save/-/tmuxinator-save-1.0.1.tgz"
   sha256 "1c79a562a37a36965d4e2ed8a1207a34e5ae22e7e45c969675c951e57c5b8a37"
 
